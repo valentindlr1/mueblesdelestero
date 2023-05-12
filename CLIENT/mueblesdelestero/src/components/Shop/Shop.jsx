f53ce5f265859bcda7ae9ffb85c1d3b6bdb77d5d
pablo herrera <valeherreradelarua@gmail.com>
2023-05-11 21:58:05 -0300

    add first components

---
+++ CLIENT/mueblesdelestero/src/components/Shop/Shop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Shop.modules.css";
+
+export default function Shop() {
+  return (
+    <div className="shop">
+      <h1>Catálogo</h1>
+      <div></div>
+    </div>
+  );
+}
