da15d1783918d2fedff7cac896a4a14c89f20587
pablo herrera <valeherreradelarua@gmail.com>
2023-05-18 04:51:07 -0300

    purchase model

--- API/models/Purchase.js
+++ API/models/Purchase.js
@@ -10,7 +10,47 @@ module.exports = (sequelize) => {
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4,
       },
-      
+      products: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: false,
+        defaultValue: []
+      },
+      buyDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      limitDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "Sucursal"
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      code: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      province: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      details: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        defaultValue: ""
+      },
+      totalPrice: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        defaultValue: 0
+      }
     },
     {
       timestamps: false,
