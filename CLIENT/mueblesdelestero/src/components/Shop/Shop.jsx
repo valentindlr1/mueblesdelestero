00b2c46dc8d2028a0e4a4eec0e5b6d1ad321cc0a
pablo herrera <valeherreradelarua@gmail.com>
2023-05-26 06:16:51 -0300

    starting to render products

--- CLIENT/mueblesdelestero/src/components/Shop/Shop.jsx
+++ CLIENT/mueblesdelestero/src/components/Shop/Shop.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.modules.css";
+import axios from "axios";
 
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+  const showProducts = products.map((p, index) => (
+    <article key={index}>
+      <h3>{p.name}</h3>
+      <p>{p.price}</p>
+      <img src={p.picture}></img>
+    </article>
+  ));
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/products")
+    .then(res=> res.data)
+    .then(prods => setProducts(prods))
+  }, []);
   return (
     <div className="shop">
       <h1>Catálogo</h1>
-      <div></div>
+      <div>{showProducts}</div>
     </div>
   );
 }
