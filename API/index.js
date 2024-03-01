282efd83dd77d0b0febe9ce5ef9a3ce378038a61
Valentin <valeherreradelarua@gmail.com>
2024-02-29 23:39:56 -0300

    db force false

--- API/index.js
+++ API/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001
 // } = require('./src/handlers/saveData.js')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   app.listen(port, async () => {
 //   await saveDogs(),
 //   saveUsers(),
